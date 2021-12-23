826b0505b33991c8a6a2b5fc55607b803f5a81cc
anton.bruhanov <anton.bruhanov@hotmail.com>
2021-12-23 15:37:06 +0300

    add ui controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,101 @@ namespace Calc
         /// </summary>
         private void InitializeComponent()
         {
+            this.textExpression = new System.Windows.Forms.TextBox();
+            this.listResults = new System.Windows.Forms.ListView();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.columnHeaderExpression = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderResult = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textExpression
+            // 
+            this.textExpression.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textExpression.Location = new System.Drawing.Point(3, 3);
+            this.textExpression.Multiline = true;
+            this.textExpression.Name = "textExpression";
+            this.textExpression.Size = new System.Drawing.Size(414, 218);
+            this.textExpression.TabIndex = 0;
+            // 
+            // listResults
+            // 
+            this.listResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderExpression,
+            this.columnHeaderResult});
+            this.listResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listResults.HideSelection = false;
+            this.listResults.Location = new System.Drawing.Point(0, 0);
+            this.listResults.Name = "listResults";
+            this.listResults.Size = new System.Drawing.Size(420, 276);
+            this.listResults.TabIndex = 1;
+            this.listResults.UseCompatibleStateImageBehavior = false;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.listResults);
+            this.splitContainer.Panel1MinSize = 100;
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.buttonCalculate);
+            this.splitContainer.Panel2.Controls.Add(this.textExpression);
+            this.splitContainer.Panel2MinSize = 100;
+            this.splitContainer.Size = new System.Drawing.Size(420, 552);
+            this.splitContainer.SplitterDistance = 276;
+            this.splitContainer.SplitterWidth = 8;
+            this.splitContainer.TabIndex = 2;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCalculate.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonCalculate.Location = new System.Drawing.Point(313, 227);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(95, 29);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(420, 552);
+            this.Controls.Add(this.splitContainer);
+            this.MinimumSize = new System.Drawing.Size(100, 300);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calc";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textExpression;
+        private System.Windows.Forms.ListView listResults;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.ColumnHeader columnHeaderExpression;
+        private System.Windows.Forms.ColumnHeader columnHeaderResult;
     }
 }
 
