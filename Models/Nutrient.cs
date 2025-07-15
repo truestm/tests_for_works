630ffa409f6c43115b21a6ed3809cd3067a619c8
Anton Bruhanov <anton.bruhanov@hotmail.com>
2025-07-15 16:23:18 +0300

    добавлена EF, и модель данных

---
+++ Models/Nutrient.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biogenom_test.Models
+{
+    [Table("Nutrients")]
+    public class Nutrient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public required string Name { get; set; }
+
+        [Required]
+        public required NutrientUnit Unit { get; set; } = NutrientUnit.Gram;
+
+        [InverseProperty("Nutrient")]
+        public virtual ICollection<ProductNutrient> ProductContents { get; set; } = new List<ProductNutrient>();
+
+        [InverseProperty("Nutrient")]
+        public virtual ICollection<NutritionalNorm> Norms { get; set; } = new List<NutritionalNorm>();
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biogenom_test.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public required string Name { get; set; }
+
+        [InverseProperty("Product")]
+        public virtual ICollection<ProductNutrient> Nutrients { get; set; } = new List<ProductNutrient>();
+
+        [InverseProperty("Product")]
+        public virtual ICollection<UserProductConsumption> ConsumedByUsers { get; set; } = new List<UserProductConsumption>();
+    }
+}
