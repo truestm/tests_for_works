8fa50be9432e1b5f3a33140c9bcff7b5398acc36
anton.bruhanov <anton.bruhanov@hotmail.com>
2025-08-11 17:54:39 +0300

    prepare to merge

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Calc
 {
+    public class A
+    {
+        public int x = 100500;
+        public A()
+        {
+
+        }
+    }
+
     static class Program
     {
         /// <summary>
@@ -11,6 +21,8 @@ namespace Calc
         [STAThread]
         static void Main()
         {
+            var a = new A[] { };
+            var first = a.FirstOrDefault();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm(new Evaluator()));
